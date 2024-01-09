0260f66dd5aa2da0e99a26636b975210ce6b71ae
Sophie Walden <seamus.walden@maine.edu>
2024-01-08 22:57:21 -0500

    title change

--- incrementally-dense-kittens/src/App.js
+++ incrementally-dense-kittens/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 import TabManager from "./components/tabManager.jsx"
 
   
 
 function App() {
-
+  useEffect(() => {
+    document.title = 'Incrementally Dense Kittens';
+  }, []);
   return (
     
     <div className="App">
